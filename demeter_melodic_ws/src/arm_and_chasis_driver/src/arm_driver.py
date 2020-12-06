0a4ad9b0a7866118d2b438dc20130062c447c5a7
Kevin Lewis <siweljk@gmail.com>
2020-12-06 13:59:02 -0800

    add submodule for kinova jaco repo and driver node file

---
+++ demeter_melodic_ws/src/arm_and_chasis_driver/src/arm_driver.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import rospy
+
+class jacoDriver():
+
+    def __init__(self):
+        #act = rospy.Publisher('instructions', )
+    
+
+if __name__ == '__main__':
+    rospy.init_node('jacoDriver')
+    
+    try:
+
+    except rospy.ROSInterruptException:
+        pass
