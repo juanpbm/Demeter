060c39a1da6a56b13081e9eecd615f0fd09308e8
juanpbm <31747012+juanpbm@users.noreply.github.com>
2021-02-15 20:54:00 -0500

    contours

--- demeter_ws/src/vision/scripts/segment.py
+++ demeter_ws/src/vision/scripts/segment.py
@@ -9,11 +9,11 @@ def thresh_image(img):
     #Bluring 
     kernel = np.ones((5,5),np.float32)/25
     imgblur = cv2.filter2D(img,-1,kernel)
-    imgblured = cv2.cvtColor(imgblur, cv2.COLOR_BGR2HSV)
+    imgblured = cv2.cvtColor(imgblur, cv2.COLOR_RGB2HSV)
     
-    #thresholding
-    mask_lower = cv2.inRange(imgblured,(0,120,70),(30,255,255))
-    mask_upper = cv2.inRange(imgblured,(120,120,70),(180,250,250))
+    #Thresholding
+    mask_lower = cv2.inRange(imgblured,(0,120,70),(10,255,255))
+    mask_upper = cv2.inRange(imgblured,(165,120,70),(180,250,250))
     mask = mask_lower+mask_upper
 
     result = cv2.bitwise_and(imgblured, imgblured, mask=mask)
@@ -25,7 +25,7 @@ def thresh_image(img):
 
 def pepper_finder(img, imgt):
     #find edges
-    edges = cv2.Canny(imgt, 100, 500)
+    edges = cv2.Canny(imgt, 800, 50, L2gradient = True)
     #plt.imshow(edges)
     #plt.show()
 
@@ -43,51 +43,67 @@ def pepper_finder(img, imgt):
 
     coord = [left,top,right,bottom]
     cv2.rectangle(img,(left,top),(right,bottom),(0,255,0),2)
-    fig, ax = plt.subplots()
-    ax.imshow(img)
+    plt.imshow(img)
     plt.show()
+
     return (coord)
 
 def contour_finder(img, coord):
     #TODO return bool if there is only one countour inside else return how many and the coords
     crop_img = img[coord[1]:coord[3], coord[0]:coord[2]]
-    plt.imshow(crop_img)
-    plt.show()
+    #plt.imshow(crop_img)
+    #plt.show()
 
-    crop_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-    ret,thresh = cv2.threshold(crop_img,100,255,0)
-    contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    print(len(contours))
-    include = []
-    for c in contours:
-        if cv2.contourArea(c) > 700:
-            include.append(c)
-            
-    imga = cv2.drawContours(crop_img, include, -1, (0,255,0), 3)
+    crop_img_gry = cv2.cvtColor(crop_img, cv2.COLOR_RGB2GRAY)
+    ret,thresh = cv2.threshold(crop_img_gry,50,255,0)
+    contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_TC89_L1)
+     
+    pepper_cont = []
+    threshold = (len(imgt)*len(imgt[0])*0.01)
+
+    for i in range(0,len(contours)):
+        if hierarchy[0,i,3] == -1 and cv2.contourArea(contours[i]) > threshold:
+            pepper_cont.append(contours[i])
+    
+    print(len(pepper_cont))       
+    imga = cv2.drawContours(crop_img, pepper_cont, -1, (0,255,0), 3)
     plt.imshow(imga)
     plt.show()
 
+    #TODO what are the return values and how to manga error 
+    if len(pepper_cont) == 0:
+        return(-1,-1)
+    elif len(pepper_cont) == 1:
+        return(True, pepper_cont)
+    else:
+        #TODO what can be done if many 
+        return(False, pepper_cont)
+
+
 if __name__ == "__main__":
 
-    for i in os.listdir('ltest/'):
-        img = cv2.cvtColor(cv2.imread("ltest/" + i), cv2.COLOR_BGR2RGB)
-        plt.imshow(img)
-        plt.show()
-
+    for i in os.listdir('test/'):
+        img = cv2.cvtColor(cv2.imread("test/" + i), cv2.COLOR_BGR2RGB)
         imgt = thresh_image(img)
-        coord = pepper_finder(img, imgt)
-        
-        #contours to see if there are multiple
-        #contour_finder(imgt,coord)
-        
-        if coord[0] < 10:
-            print ("move camera to the left")
-        elif coord[1] < 10:
-            print("move camera up")
-        elif coord[2] > len(img[0])-10:
-            print("move camera to the right")
-        elif coord[3] > len(img)-10:
-            print("move camera down")
+
+        print(np.count_nonzero(imgt >0)/3)
+        if np.count_nonzero(imgt > 0) >= (len(imgt)*len(imgt[0])*0.2):
+            print('there may be peppers')
+
+            coord = pepper_finder(img, imgt)
+            #contours to see if there are multiple
+            single_pepper, pepper_contour = contour_finder(imgt,coord)
+            
+            if coord[0] < 10:
+                print ("move camera to the left")
+            elif coord[1] < 10:
+                print("move camera up")
+            elif coord[2] > len(img[0])-10:
+                print("move camera to the right")
+            elif coord[3] > len(img)-10:
+                print("move camera down")
+            else:
+                print("the pepper is in the picture")
+                #ML model to see if the peper is ready
         else:
-            print("the pepper is in the picture")
-            #ML model to see if the peper is ready
+            print("no pepper here")
