d851e7efb0747cff7d82f6913ce0040f513491e2
Ubuntu <ubuntu@ubuntu.mshome.net>
2021-03-04 01:18:03 +0000

    camera driver working:

--- demeter_ws/src/vision/scripts/camera_driver.py
+++ demeter_ws/src/vision/scripts/camera_driver.py
@@ -4,6 +4,8 @@ import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 import os
+import subprocess 
+
 from vision.msg import image_Pair
 from sensor_msgs.msg import CompressedImage
 
@@ -14,65 +16,38 @@ from datetime import datetime
 
 class Camera_Driver_node:
 
-    def __init__(self):
-        #camera set up 
-        self.filename = './scenes/photo.png'
+    def __init__(self, out_path, res):
+        self.out_path = out_path 
+        self.res = res #in the from of #x#
+        self.command = "sudo fswebcam -r " + res + " -S 40 " + out_path 
 
-        # Camera settimgs
-        self.cam_width = 1280
-        self.cam_height = 480
-
-        # Final image capture settings
-        slef.scale_ratio = 0.5
-
-        # Camera resolution height must be dividable by 16, and width by 32
-        self.cam_width = int((cam_width+31)/32)*32
-        self.cam_height = int((cam_height+15)/16)*16
-        print ("Used camera resolution: "+str(cam_width)+" x "+str(cam_height))
-
-        # Buffer for captured image settings
-        self.img_width = int (cam_width * scale_ratio)
-        self.img_height = int (cam_height * scale_ratio)
-        self.capture = np.zeros((img_height, img_width, 4), dtype=np.uint8)
-        print ("Scaled image resolution: "+str(img_width)+" x "+str(img_height))
-
-        # Initialize the camera
-        self.camera = PiCamera(stereo_mode='side-by-side',stereo_decimate=False)
-        self.camera.resolution=(cam_width, cam_height)
-        self.camera.framerate = 20
-        self.camera.hflip = True
-
-
-        self.t2 = datetime.now()
-        self.counter = 0
-        self.avgtime = 0
-    
-    def video_Capture():
+    def video_Capture(self):
         #TODO capture of video and processing of frame
-        for frame in camera.capture_continuous(capture, format="bgra", use_video_port=True, resize=(img_width,img_height)):
-            counter+=1
-            t1 = datetime.now() 
-            timediff = t1-t2
-            avgtime = avgtime + (timediff.total_seconds())
-            cv2.imshow("pair", frame)
-            key = cv2.waitKey(1) & 0xFF
-            t2 = datetime.now()
-                
-            # if the `q` key was pressed, break from the loop and save last image    
-            if key == ord("q") :
-                avgtime = avgtime/counter
-                print ("Average time between frames: " + str(avgtime))
-                print ("Average FPS: " + str(1/avgtime))
-                if (os.path.isdir("./scenes")==False):
-                    os.makedirs("./scenes")
-                    cv2.imwrite(filename, frame)                        
-                    break
+        
+        self.commad += "video.jpg"
+        red = 0
+        while not red:
+            process = subprocess.Popen(self.command.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+            img = cv2.imread(self.out_path+"video.jpg")
+            img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+            red = self.red_Finder(img)
+            plt.imshow(img)
+            plt.show(block=False)
+            plt.pause(0.5)
+        print("pepper found exiting video capture")
+            
+    def img_Capture(self):
+        #TODO capture image and senf it backi
+        self.command += "img.jpg"
+        process = subprocess.Popen(self.command.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+        img = cv2.imread(self.out_path+"img.jpg")
+        plt.imshow(img)
+        plt.show(block=False)
+        plt.pause(0.5)
 
-    def img_Capture():
-        #TODO capture image and senf it back
-        pass 
-
-    def red_Finder(img):
+    def red_Finder(self, img):
 
         #Bluring 
         kernel = np.ones((5,5),np.float32)/25
@@ -88,8 +63,17 @@ class Camera_Driver_node:
         thresh_img = cv2.cvtColor(thresh_img, cv2.COLOR_HSV2RGB)        
         #plt.imshow(result)    
         #plt.show()
-        return(thresh_img)   
+        if np.count_nonzero(thresh_img > 0) >= (len(thresh_img)*len(thresh_img[0])*0.2):
+            print('there may be peppers')
+            return(True)
+        else:
+            print("no red")
+            return(False)   
     
+    def split_Img(slef,img):
+        #TODO insert split function here
+        #return(img_L, img_R)
+        pass
 
     def gen_Image_Pair(img_L, img_R, coords):
                 
---
+++ demeter_ws/src/vision/scripts/camera_test.py
@@ -0,0 +1,5 @@
+from camera_driver import Camera_Driver_node  as CMD
+
+camera = CMD("camera_out/", "640x480")
+camera.video_Capture()
+camera.img_Capture()
