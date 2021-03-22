7c196bdf43a702419a7ec1828edbc420711cb95a
AmieRamie <78973586+AmieRamie@users.noreply.github.com>
2021-03-22 19:31:54 -0400

    Update FrameMovement.py

--- demeter_ws/src/vision/scripts/FrameMovement.py
+++ demeter_ws/src/vision/scripts/FrameMovement.py
@@ -86,65 +86,147 @@ def image_show(image, nrows=1, ncols=1, cmap='gray'):
     ax.imshow(image, cmap='gray')
     ax.axis('off')
     return fig, ax
+
+def find_BoundingBox(imgSection):
+    stop = False
+    contour_BB = []
+    BB_size = []
+    cutoff_pepper = []
+    try:
+        imgt = red_Finder(imgSection)
+    except:
+        stop = True
+    #Find contours
+    if not(stop):
+        pepper_cont = contour_Finder(imgt, [0,0,imgt.shape[1],imgt.shape[0]])
+        try:
+            if len(pepper_cont) == 0:
+                stop = True
+        except:
+            print('Contours found')
+        
+    if not(stop):
+        if isinstance(pepper_cont,tuple):
+            pepper_cont_temp = pepper_cont[0]
+        else:
+            pepper_cont_temp = pepper_cont
+    
+        for contour in pepper_cont_temp:
+            #print(contour.shape)
+            contour = contour.transpose(1,0,2)[0]
+            left = min(contour[:,0])
+            right = max(contour[:,0])
+            bottom = max(contour[:,1])
+            top = min(contour[:,1])
+            bb_coord = [left,top,right,bottom]
+            contour_BB.append(bb_coord)
+            BB_size.append(abs((bb_coord[2] - bb_coord[0])*(bb_coord[3]-bb_coord[1])))
+            cutoff = [False, False, False, False]
+            for coord_index in range(0,len(bb_coord)):
+                if coord_index%2 == 0:
+                    rows = np.where(contour[:,0] == bb_coord[coord_index])[0]
+                    #print(f'Rows: {rows}')
+                    if len(rows)>1:
+                        otherDim = contour[rows][:,1]
+                        #print(otherDim)
+                        if max(otherDim)-min(otherDim) >= 15:
+                            if coord_index==0:
+                                if abs(0-contour[rows][0][0])<3:
+                                    cutoff[coord_index] = True
+                            else:
+                                if abs(imgt.shape[1]-contour[rows][0][0])<3:
+                                    cutoff[coord_index] = True
+                else:
+                    rows = np.where(contour[:,1] == bb_coord[coord_index])[0]
+                    #print(f'Rows: {rows}')
+                    if len(rows)>1:
+                        otherDim = contour[rows][:,0]
+                        #print(otherDim)
+                        if max(otherDim)-min(otherDim) >= 11:
+                            if coord_index==1:
+                                if abs(0-contour[rows][0][1])<3:
+                                    cutoff[coord_index] = True
+                            else:
+                                if abs(imgt.shape[0]-contour[rows][0][1])<3:
+                                    cutoff[coord_index] = True  
+            cutoff_pepper.append(cutoff)
+    return contour_BB, cutoff_pepper, BB_size
+
 rightLeft = [0,2]
 upDown = [1,3]
 frameSize = 75
 for i in range(0,math.ceil(fullImg.shape[1]/frameSize)):
     for j in range(0,math.ceil(fullImg.shape[0]/frameSize)): 
-        initials = np.array([i*frameSize,j*frameSize,min((i+1)*frameSize,fullImg.shape[0]),min((j+1)*frameSize,fullImg.shape[1])])
+        initialValues = [i*frameSize,j*frameSize,min((i+1)*frameSize,fullImg.shape[1]),min((j+1)*frameSize,fullImg.shape[0])]
+        initials = np.array(initialValues)
         imgSeg = fullImg[initials[1]:initials[3]-1,initials[0]:initials[2]-1]
+        frameShape = fullImg.shape
         print('Moving From this Frame')
         print(initials)
         plt.imshow(imgSeg)
         plt.show()
-        movements = np.array([0,0,0,0])
-        stop = False
-        attempt = 0
-        while(((sum(movements!=0)>=1)|(attempt == 0))&(stop == False)):
-            movements = np.array([0,0,0,0])
-            try:
-                imgt = red_Finder(imgSeg)
-                #Find edges
-            except:
-                stop = True
-            try:
-                left, top, right, bottom = box_Finder(imgSeg, imgt)
-            except:
-                stop == True
-            if not(stop):
-                print(f'Attempt {attempt}')
-                attempt+=1
-                if left ==0:
-                    print('left')
-                    movements[0] = -10
-                    movements[2] = -10
-                elif  top == 0:
-                    print('top')
-                    movements[1] = -10
-                    movements[3] = -10
-                elif right == imgSeg.shape[0]-1:
-                    print('right')
-                    movements[2] = 10
-                    movements[0] = 10
-                elif  bottom == imgSeg.shape[1]-1:
-                    print('bottom')
-                    movements[3] = 10
-                    movements[1] = 10
-                print(f'Initials 1: {initials}')
-                initials = initials+movements
-                initials = np.array(list(map(lambda x: max(x,0),initials)))
-                imgSeg = fullImg[initials[1]:initials[3]-1,initials[0]:initials[2]-1]
-                print(movements)
-                print(f'Initials 2: {initials}')
-                stop = len(np.where(initials<0)[0])>0
-                cv2.imwrite(f'/Users/amieramie/Desktop/Capstone\ Computer\ Vision/testImages/{i}_{j}_try{attempt}.jpg',imgSeg)
+        contour_BB, cutoff_pepper,BB_size = find_BoundingBox(imgSeg)
+        for contour_index in range(0,len(contour_BB)):
+            print(f'Contour {contour_index}:')
+            boundingBox = np.array(contour_BB[contour_index])
+            cutoff = cutoff_pepper[contour_index]
+            size = BB_size[contour_index]
+            if ((len(boundingBox)>0)&((size>750)|(sum(cutoff)>0))):
+                #print(boundingBox)
+                correctBoundingBox = boundingBox + [-11,-11,11,11]
+                initialCutoffs = np.array(cutoff)
+                print(initialCutoffs)
+                attempt = 0
+                stop = False
+                fullPepperFound = False
+                movements = np.array([0,0,0,0])
+                print('Navigating towards contour from: ')
+                initials = np.array(initialValues)
+                imgSeg = fullImg[initials[1]:initials[3],initials[0]:initials[2]]
                 plt.imshow(imgSeg)
                 plt.show()
-        
-        if (attempt>0)&(stop==False):
-            print('Full Pepper Found')
-        else:
-            print('No Pepper in Frame')
+                while(((sum(movements!=0)>=1)|(attempt == 0))&((stop == False)&(fullPepperFound==False))):
+                    movements = np.array([0,0,0,0])
+                    attempt = attempt + 1
+                    print(f'Attempt {attempt}:')
+                    if cutoff[0] ==True:
+                        print('left')
+                        movements[0] = -10
+                        movements[2] = -10
+                    if  cutoff[1] == True:
+                        print('top')
+                        movements[1] = -10
+                        movements[3] = -10
+                    if cutoff[2] == True:
+                        print('right')
+                        movements[2] = 10
+                        movements[0] = 10
+                    if  cutoff[3] == True:
+                        print('bottom')
+                        movements[3] = 10
+                        movements[1] = 10
+                    initials = initials+movements
+                    initials = np.array(list(map(lambda x: x,initials)))
+                    print(initials)
+                    if (initials[0]<0)|(initials[1]<0)|(initials[2]>frameShape[1])|(initials[0]>frameShape[0]):
+                        stop = True
+                    elif attempt>5:
+                        stop = True
+                    else:
+                        imgSeg = fullImg[initials[1]:initials[3],initials[0]:initials[2]]
+                        plt.imshow(imgSeg)
+                        plt.show()
+                        contours, cutoffs, BB_sizes = find_BoundingBox(imgSeg)
+                        for contour_index in range(0,len(contours)):
+                            if np.array_equal((np.array(contours[contour_index])<correctBoundingBox),np.array([False,False,True,True])):
+                                mainContour = contour_index
+                        cutoff = np.array(cutoffs[mainContour])
+                        correctBoundingBox = np.array(contours) + [-11,-11,11,11]
+                        fullPepperFound = (sum(cutoff)==0)
+                if fullPepperFound:
+                    print(f'Full Pepper Found Here is the Bounding Box: {np.array(contours[mainContour])+[-5,-5,5,5]}')
+            else:
+                print('Contour was not recognized as a pepper')
 #         left, top, right, bottom = box_Finder(imgSeg, imgt)
 #         coord = [left,top,right,bottom]
 #         cv2.rectangle(imgSeg,(left,top),(right,bottom),(0,255,0),1)
