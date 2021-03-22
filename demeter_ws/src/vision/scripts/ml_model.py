ddf704297d888ea8872010a842e00f49848a82b4
juanpbm <31747012+juanpbm@users.noreply.github.com>
2021-03-22 15:07:08 -0400

    srv added

--- demeter_ws/src/vision/scripts/ml_model.py
+++ demeter_ws/src/vision/scripts/ml_model.py
@@ -1,20 +1,33 @@
 
+import cv2
+import rospy
+
+from vision.srv import ML 
+
 from keras.preprocessing import image as image_utils
 from keras.applications.imagenet_utils import decode_predictions
 from keras.applications.imagenet_utils import preprocess_input
 from keras.applications import VGG16
 
- model = VGG16(weights="imagenet")
- image = image_utils.imresize(img_L, size=(224, 224), interpolate='bilinear', channel_first=False, **kwargs)
- image = image_utils.img_to_array(image)
- image = np.expand_dims(image, axis=0)
- image = preprocess_input(image)
- preds = self.model.predict(image)
- P = decode_predictions(preds)[0]
- print(f"the pepper is in the picture with a prob of: {P[['bell_pepper'  in i for i in P].index(True)][2]}")
+def model(req):
+    model = VGG16(weights="imagenet")
+    img = decompress(req.Left_Img)
+   
+    image = image_utils.imresize(img_L, size=(224, 224), interpolate='bilinear', channel_first=False, **kwargs)
+    image = image_utils.img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    image = preprocess_input(image)
+    preds = self.model.predict(image)
+    P = decode_predictions(preds)[0]
+    print(f"the pepper is in the picture with a prob of: {P[['bell_pepper'  in i for i in P].index(True)][2]}")
 
  def decompress(Left_Img):     
     array = np.frombuffer(Left_Img.data, np.uint8)
     img = cv2.imdecode(array,cv2.IMREAD_COLOR)
-    img = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
+    img = cv2.cvtColor(imgL, cv2.COLOR_BGR2RGB)
     return(img)
+
+if __name__ == '__main__':
+    rospy.init_node('ML_Model', anonymous=True)
+    s = rospy.Service('ML_req', ML, model)
+    rospy.spin()
