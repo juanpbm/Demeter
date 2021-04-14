54cec3c38516224bb36c60bb408f145510752dd5
Kevin <44102381+Phanx1@users.noreply.github.com>
2021-04-13 21:02:00 -0400

    Delete demeter_melodic_ws directory
    not used anymore

--- demeter_melodic_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/kevinlewis/Github/demeter_melodic/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
